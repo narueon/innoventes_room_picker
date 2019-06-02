8277670ae101945596b8944cd5eb89d495b2bc8b
Vivek Singh Thakur <31071725+narueon@users.noreply.github.com>
2019-06-02 17:52:34 +0530

    Added Constraints as constants.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+
+import { MAX_IN_ROOM, MAX_PEOPLE, MAX_ROOMS, MIN_ADULTS, MIN_CHILD, MIN_ROOMS } from './constants'
 import HotelItem from './HotelItem'
 
 class App extends React.Component {
 
     state = {
-        adults: 1,
-        rooms: 1,
-        child: 0
+        adults: MIN_ADULTS,
+        rooms: MIN_ROOMS,
+        child: MIN_CHILD
     }
 
     roomDecCheck = () => {
@@ -14,11 +16,9 @@ class App extends React.Component {
         let adults = this.state.adults;
         let child = this.state.child;
         let canStay = this.state.rooms * 4;
-        if (totalPeople <= canStay){
-            console.log('okeh no problem people can stay..!!')
-        } else {
-            console.log('need to trim babies... Hahahahahahaaaa..!!!!')
-            console.log("to reduce :", (totalPeople - canStay))
+        if (totalPeople > canStay){
+            // * reduce the number of childern and adults until it matches canStay
+            // * subtract the number of children and adults 
             while(totalPeople>canStay){
                 if(child > 0){
                     child--;
@@ -29,10 +29,6 @@ class App extends React.Component {
                 }
             }
             this.setState({adults, child})
-            console.log('adults : ', adults, 'Children: ', child)
-            // * reduce the number of childern and adults until it matches canStay
-            // * subtract the number of children and adults 
-            // canStay
         }
     }
 
@@ -55,12 +51,12 @@ class App extends React.Component {
         let child = this.state.child;
         let rooms = this.state.rooms;
         let totalPeople = adults + child;
-        let canStay = rooms * 4;
+        let canStay = rooms * MAX_IN_ROOM;
         if(totalPeople > canStay){
             while(totalPeople > canStay){
-                if(rooms < 5){
+                if(rooms < MAX_ROOMS){
                     rooms++;
-                    canStay = rooms * 4;
+                    canStay = rooms * MAX_IN_ROOM;
                 } else {
                     break;
                 }
@@ -74,26 +70,26 @@ class App extends React.Component {
         let child = this.state.child;
         let rooms = this.state.rooms;
         let totalPeople = adults + child;
-        if((totalPeople < rooms * 4) && (totalPeople <= (( rooms - 1 ) * 4)) ){
+        if((totalPeople < rooms * MAX_IN_ROOM) && (totalPeople <= (( rooms - 1 ) * MAX_IN_ROOM)) ){
             rooms -= 1;
             this.setState({rooms})
         }
     }
     
     incRoom = () => {
-        if(this.state.rooms < 5){
+        if(this.state.rooms < MAX_ROOMS){
             this.setState({rooms: this.state.rooms + 1}, this.roomIncCheck)
         }
     }
 
     decRoom = () => {
-        if(this.state.rooms > 1) {
+        if(this.state.rooms > MIN_ROOMS) {
             this.setState({rooms: this.state.rooms - 1}, this.roomDecCheck)
         }
     }
 
     incAdult = () => {
-        if((this.state.adults + this.state.child) < 20){
+        if((this.state.adults + this.state.child) < MAX_PEOPLE){
             this.setState({adults: this.state.adults + 1}, this.adultsIncCheck) // ! if we increase rooms using this. 5 rooms constraint is broken. it increases to more than 5
         } else {
             console.log('disable adults increment')
@@ -101,19 +97,19 @@ class App extends React.Component {
     }
 
     decAdult = () => {
-        if(this.state.adults > 1) {
+        if(this.state.adults > MIN_ADULTS) {
             this.setState({adults: this.state.adults - 1}, this.adultDecCheck)
         }
     }
 
     incChild= () => {
-        if((this.state.adults + this.state.child) < 20){
+        if((this.state.adults + this.state.child) < MAX_PEOPLE){
             this.setState({child: this.state.child + 1}, this.adultsIncCheck) // ! if we increase rooms using this. 5 rooms constraint is broken. it increases to more than 5
         }
     }
 
     decChild= () => {
-        if(this.state.child !== 0){
+        if(this.state.child !== MIN_CHILD){
             this.setState({child: this.state.child - 1}, this.adultDecCheck)
         }
     }
@@ -121,10 +117,16 @@ class App extends React.Component {
     render () {
         return (
             <div className="ui container">
-                <div className="ui items">
-                    <div className="ui item">
-                        <i className="users icon"></i>
-                        <div>Choose number of People</div>
+                <div>
+                    <div className="ui grid container">
+                        <div className="ui row">
+                            <div className="ui one wide column">
+                                <i className="users icon"></i>
+                            </div>
+                            <div className="ui ten wide column">
+                                <div>Choose number of People</div>
+                            </div>
+                        </div>
                     </div>
                     <div className="ui segment">
                     <div>
---
+++ src/components/constants.js
@@ -0,0 +1,15 @@
+const MAX_ROOMS = 5;
+const MAX_IN_ROOM = 4;
+const MAX_PEOPLE = MAX_ROOMS * MAX_IN_ROOM;
+const MIN_ROOMS = 1;
+const MIN_ADULTS = 1;
+const MIN_CHILD = 0;
+
+export {
+    MAX_ROOMS,
+    MAX_IN_ROOM,
+    MAX_PEOPLE,
+    MIN_ROOMS,
+    MIN_ADULTS,
+    MIN_CHILD
+}
