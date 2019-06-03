9c7663b6bf72a1a05ff1533132a7f0a8e2ca1f41
Vivek Singh Thakur <31071725+narueon@users.noreply.github.com>
2019-06-03 09:41:11 +0530

    Better Grid UI

--- src/components/HotelItem.js
+++ src/components/HotelItem.js
@@ -1,26 +1,25 @@
 import React from 'react';
+import './HotelItem.css';
 
 function HotelItem (props) {
     return ( 
-            <div className="ui two column grid">
+            <div className="ui twelve column grid container">
                 <div className="ui row">
-                    <div className="ui column">
-                        <div className="ui items">
-                            <div className="ui item">
-                                <i className={props.iconName} />
-                                <div className>{props.itemName}</div>
-                            </div>
-                        </div>
+                    <div className="ui one wide column">
+                        <i className={props.iconName + " itemicon"} style={{fontSize: "1.25em", padding: "1px", color: "blue"}} />
                     </div>
-                    <div className="ui column">
-                        <div className="ui items">
-                            <div className="ui item">
-                                <i className="minus circle icon" onClick={props.dec}/>
-                                <div>{props.count}&nbsp;</div>
-                                <i className="plus circle icon" onClick={props.inc} />
-                            </div>
-                        </div>
+                    <div className="ui twelve wide column">
+                        <div className>{props.itemName}</div>
                     </div>
+                    <div className="ui one wide column">
+                        <i className="minus circle icon itemicon" style={{fontSize: "1.25em", padding: "1px", color: "blue"}} onClick={props.dec}/>
+                    </div>
+                    <div className="ui one wide column">
+                        <div>{props.count}&nbsp;</div>
+                    </div>
+                    <div className="ui one wide column">
+                        <i className="plus circle icon itemicon" style={{fontSize: "1.25em", padding: "1px", color: "red"}} onClick={props.inc} />
+                    </div> 
                 </div>
             </div>
         );
